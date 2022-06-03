36a8458347320e898e7f1ff47c382c13793c8ae3
AnthonyPeres <anthony.peresdacruz@gmail.com>
2022-06-03 13:15:55 +0200

    Change python binance library

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from binance.spot import Spot
+from binance import Client
 from json import load
 import os
 from dotenv import load_dotenv
@@ -8,8 +8,8 @@ load_dotenv()
 BINANCE_API_KEY = os.getenv('BINANCE_API_KEY')
 BINANCE_SECRET_KEY = os.getenv('BINANCE_SECRET_KEY')
 
-client = Spot()
-print(client.time())
 
-client = Spot(key=BINANCE_API_KEY, secret=BINANCE_SECRET_KEY)
-print(client.account())
+client = Client(BINANCE_API_KEY, BINANCE_SECRET_KEY)
+print("Connected to binance")
+
+print(client.get_account())
